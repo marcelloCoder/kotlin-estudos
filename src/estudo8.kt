682cd9e62fbb1f86614d0d0232dd6e40b09bcaa0
marcelloCoder <deoliveiral767@gmail.com>
2024-10-23 20:07:00 -0300

    pratica kotlin

--- src/estudo8.kt
+++ src/estudo8.kt
@@ -7,4 +7,17 @@ fun main() {
         println("A VARIAVEL E NULA")
         println(text?.length)
     }
+
+    sayHello("ALEX $text")
+    sayHello("ALEX $text")
+    sayAge(22)
+}
+
+fun sayHello(name: String){
+    println("HELLO")
+    println("$name")
+}
+
+fun sayAge(age: Int){
+    println("$age")
 }
