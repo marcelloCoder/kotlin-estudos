4b3e2cb827e9b0879ead997cf1ffd16f59f38a56
marcelloCoder <deoliveiral767@gmail.com>
2025-04-07 20:57:00 -0300

    var and val

--- src/estudo10.kt
+++ src/estudo10.kt
@@ -2,8 +2,9 @@ fun main() {
     for (i in 1..10){
         println("i $i")
     }
-    var number = 10
-    while (number < 11){
+    var number = 10 // variavel mutavel
+    val limit = 18 // variavel imutavel
+    while (number < limit){
         println("$number")
         number++
     }
