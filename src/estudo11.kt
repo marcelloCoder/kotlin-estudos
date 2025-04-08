193c6d56c0bfaa24a627b82bc829ad312d8e423c
marcelloCoder <deoliveiral767@gmail.com>
2025-04-07 21:24:06 -0300

    new var

---
+++ src/estudo11.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val nome = "marcello"
+    val numeroCalcado = 43
+    val populacaoBrasil = 212500000
+
+    println("nome : $nome")
+    println("numero calcado: $numeroCalcado")
+    println("populacao brasil : $populacaoBrasil")
+
+    var soma = 0.0
+    soma = 4.0 + 4.0
+    println(soma)
+
+    val divisao = 67.0 / 44.0
+    println(divisao)
+
+    val resto = 2 % 2
+    println(resto)
+
+    var incrementar = 99.0
+    var descrementar = 11.1
+    println(incrementar++)
+    println(++incrementar)
+    println(descrementar--)
+    println(--descrementar)
+}
